5a9d032ff572bd6ccadca44e49cc481338ec55e6
timderijck <timderijck2@gmail.com>
2026-06-05 11:23:47 +0200

    Credits werkend, styling TBD

--- the_citadel_menu.gd
+++ the_citadel_menu.gd
@@ -8,3 +8,20 @@ func _process(delta: float) -> void:
 
 func _on_button_3_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_button_4_pressed() -> void:
+	$CreditsMenu.visible = true
+	$CenterContainer/VBoxContainer/Button.visible = false
+	$CenterContainer/VBoxContainer/Button2.visible = false
+	$CenterContainer/VBoxContainer/Button3.visible = false
+	$CenterContainer/VBoxContainer/Button4.visible = false
+	
+
+
+func _on_back_pressed() -> void:
+	$CreditsMenu.visible = false
+	$CenterContainer/VBoxContainer/Button.visible = true
+	$CenterContainer/VBoxContainer/Button2.visible = true
+	$CenterContainer/VBoxContainer/Button3.visible = true
+	$CenterContainer/VBoxContainer/Button4.visible = true
