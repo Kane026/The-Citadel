37966efbc628ca24291a5368fd3cb998694fdf21
djgva <djgva@BIG-DILL-ROC>
2026-06-11 09:19:16 +0200

    Added the feature that Duke follows the player instead of walking without turning.0

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -8,7 +8,6 @@ var anim: AnimationPlayer
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
-	# Zoek automatisch de AnimationPlayer op in alle children
 	anim = _find_animation_player(self)
 
 func _find_animation_player(node: Node) -> AnimationPlayer:
@@ -39,6 +38,12 @@ func _physics_process(delta: float) -> void:
 		var direction = (target - global_position).normalized()
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
+
+		# Draai naar de speler toe
+		var look_target = player.global_position
+		look_target.y = global_position.y
+		look_at(look_target)
+		rotate_y(deg_to_rad(90))
 	else:
 		if anim:
 			anim.stop()
