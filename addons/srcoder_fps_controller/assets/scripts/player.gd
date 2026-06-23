1699f342b2b13e3e5c1d3f4366e709485e85b40f
djgva <djgva@BIG-DILL-ROC>
2026-06-23 11:32:07 +0200

    Added the feature that Duke can die because of the bullets

--- addons/srcoder_fps_controller/assets/scripts/player.gd
+++ addons/srcoder_fps_controller/assets/scripts/player.gd
@@ -1,94 +1,65 @@
 extends CharacterBody3D
 
-## The movement speed in m/s. Default is 5.
 @export_range(1.0,30.0) var speed : float = 15.0
-## The Jump Velocity in m/s- default to 6.0
 @export_range(2.0,10.0) var jump_velocity : float = 6.0
-
-## Mouse sensitivity for looking around. Default is 3.0
 @export_range(1.0,5.0) var mouse_sensitivity = 3.0
 var mouse_motion : Vector2 = Vector2.ZERO
 var pitch = 0
-
-## The amount of acceleration on the ground- less feels floaty, more is snappy-[br]Default is 4
 @export_range(1.0,10.0) var ground_acceleration := 4.0
-## the amount of acceleration when in the air. less feels more floaty more is more snappy.[br]Default is 0.5
 @export_range(0.0,5.0) var air_acceleration := 0.5
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 @export_range(5.0,25.0) var gravity : float = 15.0
+var bullets_fired: int = 0
 
-# the camera pivot for head pitch movement
 @onready var camera_pivot : Node3D = $CameraPivot
-
 @onready var knife = $CameraPivot/Camera3D/Knife
 @onready var gun = $CameraPivot/Camera3D/Gun
-
-var bullet=load("res://bullet.tscn")
+var bullet = load("res://bullet.tscn")
 @onready var barrel_position = $CameraPivot/Camera3D/Gun/barrel_position
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
 
-	# Shooting
 	if Input.is_action_just_pressed("shoot"):
 		if gun.visible:
 			var instance = bullet.instantiate()
-
 			get_tree().current_scene.add_child(instance)
-
 			instance.global_position = barrel_position.global_position
 			instance.global_transform.basis = barrel_position.global_transform.basis
+			bullets_fired += 1
+			if bullets_fired >= 10:
+				var duke = get_tree().get_first_node_in_group("dukesmog")
+				if duke:
+					duke.neem_schade(duke.max_hp)
+				bullets_fired = 0
 
-		
-
-	# Movement
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y))
-
 	var target_velocity := Vector3.ZERO
-
 	if direction:
 		target_velocity = direction
-
 	if is_on_floor():
 		velocity.x = move_toward(velocity.x, target_velocity.x * speed, speed * ground_acceleration * delta)
 		velocity.z = move_toward(velocity.z, target_velocity.z * speed, speed * ground_acceleration * delta)
 	else:
 		velocity.x = move_toward(velocity.x, target_velocity.x * speed, speed * air_acceleration * delta)
 		velocity.z = move_toward(velocity.z, target_velocity.z * speed, speed * air_acceleration * delta)
-
 	move_and_slide()
-	
-	#rotate the player and camera pivot based on mouse movement
+
 	rotate_y(-mouse_motion.x * mouse_sensitivity / 1000)
 	pitch -= mouse_motion.y * mouse_sensitivity / 1000
-	pitch = clampf(pitch,-1.35,1.35)
+	pitch = clampf(pitch, -1.35, 1.35)
 	camera_pivot.rotation.x = pitch
-	#reset it
 	mouse_motion = Vector2.ZERO
-	
 
-#handle and store mouse motion
 func _input(event: InputEvent):
 	if event is InputEventMouseMotion:
 		mouse_motion = event.relative
-
 	if event.is_action_pressed("weapon_1"):
 		knife.visible = true
 		gun.visible = false
-
 	if event.is_action_pressed("weapon_2"):
 		knife.visible = false
 		gun.visible = true
-		
-		
-		
-
-		
--- character_body_3d.gd
+++ character_body_3d.gd
@@ -2,13 +2,24 @@ extends CharacterBody3D
 
 @export var speed: float = 4.0
 @export var stop_distance: float = 1.5
+@export var max_hp: float = 100.0
 
+var hp: float = 100.0
 var player: CharacterBody3D
 var anim: AnimationPlayer
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
 	anim = _find_animation_player(self)
+
+func neem_schade(schade: float) -> void:
+	hp -= schade
+	print("DukeSmog HP: ", hp)
+	if hp <= 0:
+		sterven()
+
+func sterven() -> void:
+	queue_free()
 
 func _find_animation_player(node: Node) -> AnimationPlayer:
 	for child in node.get_children():
@@ -22,14 +33,11 @@ func _find_animation_player(node: Node) -> AnimationPlayer:
 func _physics_process(delta: float) -> void:
 	if player == null:
 		return
-
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
 	var my_pos = Vector2(global_position.x, global_position.z)
 	var player_pos = Vector2(player.global_position.x, player.global_position.z)
 	var distance = my_pos.distance_to(player_pos)
-
 	if distance > stop_distance:
 		if anim:
 			anim.play("Armature|Run_02|baselayer")
@@ -38,16 +46,13 @@ func _physics_process(delta: float) -> void:
 		var direction = (target - global_position).normalized()
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
-
-		# Draai naar de speler toe
 		var look_target = player.global_position
 		look_target.y = global_position.y
 		look_at(look_target)
-		rotate_y(deg_to_rad(90))
+		rotate_y(deg_to_rad(180))
 	else:
 		if anim:
 			anim.stop()
 		velocity.x = 0
 		velocity.z = 0
-
 	move_and_slide()
